fc7b6bae3100d27b971797f53f83eb7e81edf8fc
nnamm <t.nnamm@gmail.com>
2022-05-05 01:56:49 +0900

    Deleted unnecessary comments

--- src/libs/diffText.ts
+++ src/libs/diffText.ts
@@ -1,14 +1,9 @@
 import diff from 'simple-text-diff';
 import { DiffText } from '../contexts/GoodThingContext';
 
-// export type DiffText = {
-//   diffEnteredText: string;
-//   diffDeepLText: string;
-// };
-
 const diffText = (enteredText: string, translatedText: string): DiffText => {
   const diffResult = diff.diffPatch(enteredText, translatedText);
-  // const diffResult = diff.diffPatchBySeparator(enteredText, translatedText, '.');
+
   return {
     diffEnteredText: diffResult.before.replace(/\n/g, '<br>'),
     diffDeepLText: diffResult.after.replace(/\n/g, '<br>'),
