625a1340ed80936e4e018cc393a372689a1e4d2a
nnamm <t.nnamm@gmail.com>
2022-05-05 21:17:30 +0900

    Add icon button func

--- src/components/GoodThingContainer.tsx
+++ src/components/GoodThingContainer.tsx
@@ -1,22 +1,31 @@
+import { useState } from 'react';
 import GoodThingProvider from '../contexts/GoodThingContext';
 import TextArea from './TextArea';
 import ExecButton from './ExecButton';
 import IconButton from '@mui/material/IconButton';
-import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import ArrowRightOutlinedIcon from '@mui/icons-material/ArrowRightOutlined';
 
 const GoodThingContainer = () => {
+  const [targetShow, setTargetShow] = useState<boolean>(false);
+
+  const targetHandlerShow = () => {
+    setTargetShow(!targetShow);
+  };
+
   return (
     <div className="w-1/2">
       <GoodThingProvider>
         <TextArea name="Source" />
-        <div className="flex flex-row items-start mt-2">
-          <IconButton size="small">
-            <ArrowDropDownIcon />
+        <div className="flex flex-row items-start mt-1">
+          <IconButton size="small" onClick={() => targetHandlerShow()}>
+            <ArrowRightOutlinedIcon />
           </IconButton>
-          <div className="flex flex-col w-full ml-2">
-            <TextArea name="Target" />
-            <ExecButton name="DeepL" />
-          </div>
+          {targetShow && (
+            <div className="flex flex-col w-full ml-2">
+              <TextArea name="Target" />
+              <ExecButton name="DeepL" />
+            </div>
+          )}
         </div>
       </GoodThingProvider>
     </div>
