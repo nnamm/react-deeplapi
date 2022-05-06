f2f0bc439ec745d8704e26723f7290a51b06df01
nnamm <t.nnamm@gmail.com>
2022-05-06 12:29:57 +0900

    Changed button to close icon

--- src/components/DiffModal.tsx
+++ src/components/DiffModal.tsx
@@ -1,6 +1,7 @@
 import { useContext, useState } from 'react';
 import { GoodThingContext } from '../contexts/GoodThingContext';
-import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import HighlightOffOutlinedIcon from '@mui/icons-material/HighlightOffOutlined';
 import parser from 'html-react-parser';
 
 const DiffModal = () => {
@@ -19,21 +20,21 @@ const DiffModal = () => {
     <div>
       {show && (
         <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center" style={{ backgroundColor: 'rgba(0,0,0,0.5)' }}>
-          <div className="z-10 w-4/5 px-8 pt-8 pb-4 bg-gray-100 rounded-2xl" onClick={(e) => e.stopPropagation()}>
-            <div className="flex text-gray-800 leading-6">
-              <div className="w-1/2 mr-4">
-                <p className="text-sm">Entered text</p>
+          <div className="z-10 w-4/5 px-6 pt-8 pb-4 bg-gray-100 rounded-2xl" onClick={(e) => e.stopPropagation()}>
+            <div className="flex flex-row text-gray-800 leading-6">
+              <div className="w-1/2 px-2">
+                <p className="text-sm font-medium">Entered text</p>
                 <p className="mt-2">{parser(diffTexts.diffEnteredText)}</p>
               </div>
-              <div className="w-1/2">
-                <p className="text-sm">Translated text</p>
+              <div className="w-1/2 px-2">
+                <p className="text-sm font-medium">Translated text</p>
                 <p className="mt-2">{parser(diffTexts.diffDeepLText)}</p>
               </div>
             </div>
             <div className="flex justify-center items-center mt-8">
-              <Button className="focus:outline-none" style={{ textTransform: 'none' }} onClick={() => closeModal()}>
-                Close
-              </Button>
+              <IconButton size="small" onClick={() => closeModal()}>
+                <HighlightOffOutlinedIcon />
+              </IconButton>
             </div>
           </div>
         </div>
